c9b41e58ba90439b7498727418ddb9ffddb1f0a8
gyuminv2 <inamemin3@gmail.com>
2024-12-26 14:49:06 +0900

    FEAT : DFS example

---
+++ dfs.py
@@ -0,0 +1,22 @@
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end=' ')
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+graph = [
+    [],
+    [2, 3, 8],  # 1과 연결된 노드
+    [1, 7],     # 2와 연결된 노드
+    [1, 4, 5],  # ...
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+
+visited = [False] * 9
+
+dfs(graph, 1, visited)
