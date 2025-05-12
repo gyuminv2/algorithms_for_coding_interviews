400ca9e193e3d939166d5f3c2e0f6bfce01ccbd7
gyumin.park <48640323+gyuminv2@users.noreply.github.com>
2025-05-12 17:56:03 +0900

    [Bronze II] Title: 소수, Time: 388 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.py
@@ -0,0 +1,28 @@
+m = int(input())
+n = int(input())
+
+# 제한시간 : 1초
+# 1 ~ 10000 -> 100,000,000
+
+
+# 0 : 소수 아님, 1 : 소수
+def find_ood(target):
+    if target == 1:
+        return 0
+    if target == 2:
+        return 1
+    for i in range(2, target):
+        if target % i == 0:
+            return 0
+    return 1
+
+arr = []
+for i in range(m, n+1):
+    if find_ood(i):
+        arr.append(i)
+
+if not arr:
+    print(-1)
+else:
+    print(sum(arr))
+    print(min(arr))
